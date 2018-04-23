e40d0988f8dff2cec29ee961320f79607566759c
Devin Finzer <dfinzer2@gmail.com>
2018-04-22 17:54:46 -0700

    Proper response

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from datetime import datetime
-
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -8,15 +8,16 @@ FIRST_NAMES = ['Herbie', 'Jessica', 'Fluffles']
 LAST_NAMES = ['McPufflestein', 'McDonald']
 
 
-@app.route('/api/cryptopuff/<token_id>')
+@app.route('/api/puff/<token_id>')
 def cryptopuff(token_id):
+    token_id = int(token_id)
     num_first_names = len(FIRST_NAMES)
     num_last_names = len(LAST_NAMES)
     cryptopuff_name = "%s %s" % (FIRST_NAMES[token_id % num_first_names], LAST_NAMES[token_id % num_last_names])
-    return {
+    return jsonify({
         'name': cryptopuff_name,
         'description': "Generic puff description. This really should be customized.",
-    }
+    })
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
